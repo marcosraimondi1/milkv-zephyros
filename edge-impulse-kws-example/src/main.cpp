ae3fcad893d672a27653337f9c43a5969c248393
marcosraimondi1 <marcosraimondi1@hotmail.com>
2024-10-24 18:20:52 -0300

    inferencing with samples from ADC

--- edge-impulse-kws-example/src/main.cpp
+++ edge-impulse-kws-example/src/main.cpp
@@ -1,11 +1,35 @@
 // Zpehyr 3.1.x and newer uses different include scheme
 #include <zephyr/kernel.h>
-#include "features.h"
-#include "features1.h"
-#include "features2.h"
-#include "features3.h"
 #include "edge-impulse-sdk/classifier/ei_run_classifier.h"
-#include "edge-impulse-sdk/dsp/numpy.hpp"
+#include <zephyr/drivers/gpio.h>
+#include "adc.h"
+#include "pin.h"
+#include "timer.h"
+
+// registers
+#define ADC_PIN_MUX_OFFSET 0x0F8
+#define ADC_PIN_FUNC       3U
+
+// devicetree
+#define PIN_MUX_BASE       (uint32_t)DT_REG_ADDR(DT_NODELABEL(pinctrl))
+#define ADC_BASE           (uint32_t)DT_REG_ADDR(DT_ALIAS(myadc))
+#define TIMER_BASE         (uint32_t)DT_REG_ADDR(DT_ALIAS(mytimer))
+#define TIMER_IRQN         DT_IRQN(DT_ALIAS(mytimer))
+#define TIMER_IRQ_PRIORITY DT_IRQ(DT_ALIAS(mytimer), priority)
+#define LED0_NODE          DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+// constants
+#define TIMER_ID          5
+#define NSAMPLES          16000
+#define TIMER_CLK_FREQ_HZ 25000000
+#define SAMPLING_FREQ_HZ  16000
+#define TIMER_COUNT       (TIMER_CLK_FREQ_HZ / SAMPLING_FREQ_HZ) - 1
+
+// variables
+static K_SEM_DEFINE(samples_ready, 0, 1);
+static float features[NSAMPLES] = {0};
+static int count = 0;
 
 int raw_feature_get_data(size_t offset, size_t length, float *out_ptr)
 {
@@ -13,49 +37,61 @@ int raw_feature_get_data(size_t offset, size_t length, float *out_ptr)
 	return 0;
 }
 
-int raw_feature1_get_data(size_t offset, size_t length, float *out_ptr)
-{
-	memcpy(out_ptr, features1 + offset, length * sizeof(float));
-	return 0;
-}
-
-int raw_feature2_get_data(size_t offset, size_t length, float *out_ptr)
-{
-	memcpy(out_ptr, features2 + offset, length * sizeof(float));
-	return 0;
-}
-
-int raw_feature3_get_data(size_t offset, size_t length, float *out_ptr)
-{
-	memcpy(out_ptr, features3 + offset, length * sizeof(float));
-	return 0;
-}
+void adc_init();
+void timer_init(uint32_t load_count);
+void irq_config();
 
 int main()
 {
-	// This is needed so that output of printf is output immediately without buffering
-	setvbuf(stdout, NULL, _IONBF, 0);
-
 	printk("Edge Impulse standalone inferencing (Zephyr)\n");
+	setvbuf(stdout, NULL, _IONBF, 0);
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
-	if (sizeof(features3) / sizeof(float) != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+	if (sizeof(features) / sizeof(float) != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
 		printk("The size of your 'features' array is not correct. Expected %d items, but "
 		       "had %lu\n",
-		       EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, sizeof(features3) / sizeof(float));
+		       EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, sizeof(features) / sizeof(float));
 		return 1;
 	}
 
 	ei_impulse_result_t result = {0};
 
 	while (1) {
-		// the features are stored into flash, and we don't want to load everything into RAM
+		printk("Sampling in \n3\n");
+		gpio_pin_set_dt(&led, 1);
+		k_sleep(K_MSEC(500));
+		gpio_pin_set_dt(&led, 0);
+		k_sleep(K_MSEC(500));
+		printk("2\n");
+		gpio_pin_set_dt(&led, 1);
+		k_sleep(K_MSEC(500));
+		gpio_pin_set_dt(&led, 0);
+		k_sleep(K_MSEC(500));
+		printk("1\n");
+		gpio_pin_set_dt(&led, 1);
+		k_sleep(K_MSEC(500));
+		gpio_pin_set_dt(&led, 0);
+		k_sleep(K_MSEC(500));
+		gpio_pin_set_dt(&led, 1);
+		printk("NOW!\n");
+
+		adc_init();
+		timer_init(TIMER_COUNT);
+		irq_config();
+
+		while (count < NSAMPLES) {
+			k_sleep(K_MSEC(100));
+		}
+		count = 0;
+		gpio_pin_set_dt(&led, 0);
+
 		signal_t features_signal;
-		features_signal.total_length = sizeof(features3) / sizeof(features3[0]);
-		features_signal.get_data = &raw_feature3_get_data;
+		features_signal.total_length = sizeof(features) / sizeof(features[0]);
+		features_signal.get_data = &raw_feature_get_data;
 
 		// invoke the impulse
-		EI_IMPULSE_ERROR res = run_classifier(&features_signal, &result, true);
-		printk("run_classifier returned: %d\n", res);
+		printk("Running the impulse...\n");
+		EI_IMPULSE_ERROR res = run_classifier(&features_signal, &result, false);
 
 		if (res != 0) {
 			return 1;
@@ -68,7 +104,93 @@ int main()
 			printk("    %s: %f\n", result.classification[ix].label,
 			       (double)result.classification[ix].value);
 		}
-
-		k_msleep(2000);
 	}
+}
+
+void timer_stop()
+{
+	// disable timer
+	hal_timer_disable_clk(TIMER_ID);
+	TIMER_CTRL(TIMER_BASE) &= ~1;
+}
+
+void adc_stop()
+{
+	hal_adc_reset_sel_channel(ADC_BASE, ADC_CHANNEL_SEL_Msk);
+	hal_adc_stop(ADC_BASE);
+	hal_adc_disable_clk();
+}
+
+static void timer_isr()
+{
+	static bool first = true;
+	if (TIMER_INTR_STA(TIMER_BASE) & 1) {
+		volatile int32_t a = TIMER_INTR_CLR(TIMER_BASE);
+		ARG_UNUSED(a); // clear interrupt
+
+		if (first) {
+			first = false;
+			hal_adc_start(ADC_BASE); // start conversion
+			return;
+		}
+
+		if (hal_adc_get_data_ready(ADC_BASE)) {
+			features[count] = hal_adc_get_channel1_data(ADC_BASE) & 0xFFF;
+
+			if (hal_adc_get_channel1_data_valid(ADC_BASE) == 0) {
+				printk("ADC data is invalid\n");
+				while (1) {
+				}
+			}
+
+			count += 1;
+			if (count >= NSAMPLES) {
+				timer_stop();
+				adc_stop();
+				printk("ADC Finished Sampling\n");
+				return;
+			}
+			hal_adc_start(ADC_BASE); // start conversion
+		} else {
+			printk("ADC data is not ready\n");
+			while (1) {
+			}
+		}
+	}
+	return;
+}
+
+void irq_config(void)
+{
+	// Conectar la interrupcion (agrega a la tabla de interrupciones)
+	IRQ_CONNECT(TIMER_IRQN, TIMER_IRQ_PRIORITY, timer_isr, NULL, 0);
+	irq_enable(TIMER_IRQN);
+}
+
+void timer_init(uint32_t load_count)
+{
+	// peripheral clock is 25MHz, 1s = 25M counts
+	// disable timer
+	hal_timer_disable_clk(TIMER_ID);
+	TIMER_CTRL(TIMER_BASE) &= ~1;
+	volatile int32_t a = TIMER_INTR_CLR(TIMER_BASE);
+	ARG_UNUSED(a);
+
+	// config timer
+	TIMER_LOAD_COUNT(TIMER_BASE) = load_count;
+	TIMER_CTRL(TIMER_BASE) &= ~(1 << 2); // interrupt not masked
+	TIMER_CTRL(TIMER_BASE) |= 1 << 1;    // user-defined mode
+
+	// enable timer
+	hal_timer_enable_clk(TIMER_ID);
+	TIMER_CTRL(TIMER_BASE) |= 1;
+}
+
+void adc_init()
+{
+	uint8_t ch_id = 1;
+	pin_set_mux(PIN_MUX_BASE + ADC_PIN_MUX_OFFSET, ADC_PIN_FUNC);
+	hal_adc_enable_clk();
+	hal_adc_set_sel_channel(ADC_BASE, (uint32_t)1U << (ADC_CTRL_ADC_SEL_Pos + ch_id));
+	hal_adc_cyc_setting(ADC_BASE);
 }
---
+++ edge-impulse-kws-example/src/timer.h
@@ -0,0 +1,40 @@
+#ifndef __CVI_TIMER_H__
+#define __CVI_TIMER_H__
+
+#include <stdint.h>
+
+struct cvi_timer_regs_t {
+	uint32_t LOAD_COUNT;
+	uint32_t CURRENT_VALUE;
+	uint32_t CTRL;
+	uint32_t INTR_CLR;
+	uint32_t INTR_STA;
+};
+static struct cvi_timer_regs_t timer_offsets = {
+	.LOAD_COUNT = 0x0,
+	.CURRENT_VALUE = 0x4,
+	.CTRL = 0x8,
+	.INTR_CLR = 0xC,
+	.INTR_STA = 0x10,
+};
+
+static struct cvi_timer_regs_t *timer_reg = &timer_offsets;
+
+#define TIMER_LOAD_COUNT(reg_base)    *((uint32_t *)(reg_base + timer_reg->LOAD_COUNT))
+#define TIMER_CURRENT_VALUE(reg_base) *((uint32_t *)(reg_base + timer_reg->CURRENT_VALUE))
+#define TIMER_CTRL(reg_base)          *((uint32_t *)(reg_base + timer_reg->CTRL))
+#define TIMER_INTR_CLR(reg_base)      *((uint32_t *)(reg_base + timer_reg->INTR_CLR))
+#define TIMER_INTR_STA(reg_base)      *((uint32_t *)(reg_base + timer_reg->INTR_STA))
+
+#define TIMER_CLK *((uint32_t *)(0x300200C))
+
+static inline void hal_timer_enable_clk(uint8_t id)
+{
+	TIMER_CLK |= (1U << (9 + id));
+}
+
+static inline void hal_timer_disable_clk(uint8_t id)
+{
+	TIMER_CLK &= ~(1U << (9 + id));
+}
+#endif // __CVI_TIMER_H__
